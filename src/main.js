3adfef64db60a9cbbee19148a6ee7c69fbd14bcf
unknown <fabioflauzino@hotmail.com>
2023-01-18 01:30:11 -0300

    Finalizando aplicação

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./css/index.css"
 import IMask from "imask"
+import Swal from "sweetalert2"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -63,14 +64,56 @@ const cardNumberPattern = {
       cardtype: "default",
     }
   ],
-  dispatc: function (appended, dynamicMasked){
+  dispatc: function (appended, dynamicMasked) {
     const number = (dynamicMasked.value + appended).replace(/\D/g, "")
-    const foundMask = dynamicMasked.compiledMask.fing((item)=> {
+    const foundMask = dynamicMasked.compiledMask.fing((item) => {
       return number.match(item.regex)
     })
 
     return foundMask
   }
 }
-
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  const addButton = document.querySelector("#add-card")
+
+  e.preventDefault()
+  addButton.disabled = true
+  addButton.innerText = "ENVIANDO..."
+  
+
+  setTimeout(() => {
+    Swal.fire({
+      icon: 'success',
+      title: 'Parabéns!',
+      text: 'Seu cartão foi adicionado com sucesso!',
+    })
+    addButton.disabled = false
+    addButton.innerText = "ADICIONAR CARTÃO"
+  }, 2000)
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerText = cardHolder.value.length === 0 ? "NOME DO TTITULAR" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", function () {
+  const ccSecurity = document.querySelector(".cc-security .value")
+  ccSecurity.innerText = securityCodeMasked.value.length === 0 ? "123" : securityCodeMasked.value
+})
+
+cardNumberMasked.on("accept", function () {
+  const ccSecurity = document.querySelector(".cc-number")
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  ccSecurity.innerText = cardNumberMasked.value.length === 0 ? "1234 5678 9012 3456" : cardNumberMasked.value
+})
+
+expirationDateMasked.on("accept", function () {
+  const ccSecurity = document.querySelector(".cc-extra .value")
+  ccSecurity.innerText = expirationDateMasked.value.length === 0 ? "01/32" : expirationDateMasked.value
+})
